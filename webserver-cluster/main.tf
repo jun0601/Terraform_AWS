3ed6f49558e7ee8a4d047156f65154d52c2cd4cc
JunHyeong Lee <bljh5220@naver.com>
2026-05-08 17:07:44 +0900

    Add webserver-cluster Project

--- webserver-cluster/main.tf
+++ webserver-cluster/main.tf
@@ -1,23 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.50, < 6.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "ap-northeast-2"
-  
-  # 2.x 버전의 AWS 공급자 허용
-  version = "~> 2.0"
+  region = "ap-northeast-2"
 }
 
 resource "aws_launch_template" "example" {
-  name_prefix = "example-"
-  image_id        = "ami-0d4c056a16f3ae150"
-  instance_type   = "t3.micro"
-  
+  name_prefix   = "example-"
+  image_id      = data.aws_ssm_parameter.al2023.value
+  instance_type = "t3.micro"
+
   vpc_security_group_ids = [aws_security_group.instance.id]
   key_name = "JH_Keypair"
-  user_data = <<-EOF
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               dnf install -y httpd
               echo "Hello, World Server Port is ${var.server_port}" > /var/www/html/index.html
               systemctl enable --now httpd
               EOF
+  )
 
   # 오토스케일링 그룹과 함께 시작 구성을 사용할 때 필요합니다.
   lifecycle {
@@ -61,6 +68,103 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+
+  name               = var.alb_name
+
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern{
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+
+  name = var.alb_name
+
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_security_group" "alb" {
+
+  name = var.alb_security_group_name
+
+  # HTTP 인바운드 트래픽 허용
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # 모든 아웃바운트 트래픽 허용
   egress {
     from_port   = 0
     to_port     = 0
--- webserver-cluster/variables.tf
+++ webserver-cluster/variables.tf
@@ -9,3 +9,21 @@ variable "security_group_name" {
   type        = string
   default     = "terraform-example-instance"
 }
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-example"
+}
+
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
+}
